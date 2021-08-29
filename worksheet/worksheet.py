98300f41bfad11758a3db378a0d770080bed9489
zakum1 <james.nicolson.za@gmail.com>
2021-08-28 22:18:46 -0700

    Implemented rendering of current state

--- worksheet/worksheet.py
+++ worksheet/worksheet.py
@@ -3,7 +3,7 @@
 import pkg_resources
 from web_fragments.fragment import Fragment
 from xblock.core import XBlock
-from xblock.fields import Dict, Scope
+from xblock.fields import Dict, Scope, XMLString
 import logging;
 from lxml import etree, html
 from io import StringIO
@@ -23,6 +23,12 @@ class WorksheetBlock(XBlock):
         help="A map of the user responses on the worksheet",
     )
 
+    html = XMLString(
+        default="<div></div>",
+        scope=Scope.content,
+        help="HTML fragment for the worksheet structure",
+    )
+
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
@@ -34,9 +40,9 @@ class WorksheetBlock(XBlock):
         The primary view of the WorksheetBlock, shown to students
         when viewing courses.
         """
-
+        print(self.html)
         log.info('WorksheetBlock.studentView')
-        html_ws = '<div id="worksheet">' + self.resource_string("static/html/worksheet.html") + '</div>'
+        html_ws = '<div id="worksheet">' + self.html + '</div>'
 
         tree   = html.fragment_fromstring(html_ws)
         inputs = tree.xpath("//*[contains(concat(' ', @class, ' '), ' input ')]")
@@ -49,7 +55,6 @@ class WorksheetBlock(XBlock):
         # we use c14n (canonical) to prevent <div></div> being collapsed to <div/>
         # because it causes strange behaviour in the XBlock
         htmlWithResponses = etree.tostring(tree, method="c14n", pretty_print=True).decode("utf-8")
-        print(htmlWithResponses)
 
         frag = Fragment(htmlWithResponses.format(self=self))
         frag.add_css(self.resource_string("static/css/worksheet.css"))
@@ -76,13 +81,28 @@ class WorksheetBlock(XBlock):
         """A canned scenario for display in the workbench."""
         return [
             ("WorksheetBlock",
-             """<worksheet/>
-             """),
-            ("Multiple WorksheetBlock",
-             """<vertical_demo>
-                <worksheet/>
-                <worksheet/>
-                <worksheet/>
-                </vertical_demo>
+             """
+<worksheet>
+<div>
+    <div class="xbt-table">
+            <div class="xbt-row">
+                <div class="xbt-cell header"></div>
+                <div class="xbt-cell header">Column 1</div>
+                <div class="xbt-cell header">Column 2</div>
+            </div>
+            <div class="xbt-row">
+                <div class="xbt-cell header">Row 1</div>
+                <div class="xbt-cell static">Example 1</div>
+                <div class="xbt-cell static">Example 2</div>
+            </div>
+            <div class="xbt-row repeat">
+                <div class="xbt-cell header">Row 2</div>
+                <div name="example1" class="xbt-cell input">Enter test 1 here</div>
+                <div name="example2" class="xbt-cell input">Enter test 2 here</div>
+            </div>
+    </div>
+    <div id="buttons"><p>You can add and delete rows to the worksheet</p></div>
+</div>
+</worksheet>
              """),
         ]
